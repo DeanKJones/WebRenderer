14189ddd81ea5567d065f0c127e7688530079367
Dean <dhkjones@gmail.com>
2024-01-20 12:24:54 +0100

    Add subtract and multiply methods to Vec2 class

--- src/utils/math/Vec2.ts
+++ src/utils/math/Vec2.ts
@@ -8,6 +8,20 @@ export class Vec2 extends Float32Array
         this[1] = y;
     }
 
+    public subtract(v1: Vec2): Vec2
+    {
+        this[0] -= v1[0];
+        this[1] -= v1[1];
+        return this;
+    }
+
+    public multiply(scalar: number): Vec2
+    {
+        this[0] *= scalar;
+        this[1] *= scalar;
+        return this;
+    }
+
     public get x(): number
     {
         return this[0];
