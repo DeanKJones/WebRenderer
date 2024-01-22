404078d7f35ab55f4234ce0f4ba02b19fdaf56e6
Dean <dhkjones@gmail.com>
2024-01-22 16:00:51 +0100

    Add support for parsing and storing normals in OBJ files

--- src/scene/geometry/import/ImportOBJ.ts
+++ src/scene/geometry/import/ImportOBJ.ts
@@ -7,6 +7,9 @@ export class MeshOBJ
     private parsed_vertices!: Float32Array;
     private parsed_indices!: Uint16Array;
 
+    private parsed_normals?: Float32Array;
+    private parsed_normal_indices?: Uint16Array;
+
     constructor(objFile: string)
     {
         this.parseOBJ(objFile);
@@ -17,8 +20,9 @@ export class MeshOBJ
     public initData(): GeometryData 
     {
         let vertices = this.parsed_vertices;
-
         let indices = this.parsed_indices;
+        let normals = this.parsed_normals;
+        let normalIndices = this.parsed_normal_indices;
 
         let colors = new Float32Array([1,1,1,1]);
 
@@ -28,6 +32,8 @@ export class MeshOBJ
         let cubeGeometryData = {
             positions: vertices,
             indices: indices,
+            // normals: normals,
+            // normalIndices: normalIndices,
             colors: colors,
             texCoords: texCoords
         }
@@ -37,6 +43,10 @@ export class MeshOBJ
     // Parse OBJ
     public parseOBJ(objString: string)
     {
+
+        //---------------------------------------------
+        // Vertices
+
         let vertexMatch: any;
         let vertexPattern = /^v\s+(\S+)\s+(\S+)\s+(\S+)$/gm;
         let vertices: number[] = [];
@@ -74,6 +84,47 @@ export class MeshOBJ
             indices.push(indexData);
         }
         this.parsed_indices = new Uint16Array(indices);
-    }
 
+
+        //---------------------------------------------
+        // Normals 
+
+        let normalMatch: any;
+        let normalPattern = /^vn\s+(\S+)\s+(\S+)\s+(\S+)$/gm;
+        let normals: number[] = [];
+
+        while ((normalMatch = normalPattern.exec(objString)) !== null) {
+            // Normal 1
+            let normalData = parseFloat(normalMatch[1]);
+            normals.push(normalData);
+
+            // Normal 2
+            normalData = parseFloat(normalMatch[2]);
+            normals.push(normalData);
+
+            // Normal 3
+            normalData = parseFloat(normalMatch[3]);
+            normals.push(normalData);
+        }
+        this.parsed_normals = new Float32Array(normals);
+
+        let normalIndexMatch: any;
+        let normalIndexPattern = /^f\s+\d+\/\S+\/(\d+)\s+\d+\/\S+\/(\d+)\s+\d+\/\S+\/(\d+)$/gm;
+        let normalIndices: number[] = [];
+
+        while ((normalIndexMatch = normalIndexPattern.exec(objString)) !== null) {
+            // Normal Index 1
+            let normalIndexData = parseInt(normalIndexMatch[1]);
+            normalIndices.push(normalIndexData);
+
+            // Normal Index 2
+            normalIndexData = parseInt(normalIndexMatch[2]);
+            normalIndices.push(normalIndexData);
+
+            // Normal Index 3
+            normalIndexData = parseInt(normalIndexMatch[3]);
+            normalIndices.push(normalIndexData);
+        }
+        this.parsed_normal_indices = new Uint16Array(normalIndices);
+    }
 }
